9b4e7dec39be5dac3f363c1d4c7cb089fdbad854
PineconeLP <pineconelp4@gmail.com>
2020-08-08 19:45:23 -0400

    Setup Nuxt configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,20 @@
 export default {
   target: "static",
-  srcDir: "src/"
+  srcDir: "src/",
+  plugins: ["@/plugins/bootstrap-vue"],
+  head: {
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "Meta description" }
+    ]
+  },
+  css: ["@/css/main.css"],
+  render: {
+    bundleRenderer: {
+      shouldPreload: (file, type) => {
+        return ["script", "style", "font"].includes(type);
+      }
+    }
+  }
 };
