72539c85807073735b2cb55dd51dbd11898d78f3
PineconeLP <pineconelp4@gmail.com>
2020-08-12 19:17:52 -0400

    Add header dropdown

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="header" class="py-3">
     <nav class="container">
-      <div class="row align-items-center">
-        <div class="col-sm justify-content-sm-start d-flex justify-content-center">
+      <div class="row align-items-center navbar-dark">
+        <div class="col-sm d-flex justify-content-between">
           <a class="nav-title" href="/">Cam Alert</a>
+          <b-navbar-toggle class="d-sm-none" target="menu">
+            <span class="navbar-toggler-icon"></span>
+          </b-navbar-toggle>
         </div>
-        <div class="col-sm-auto my-3 justify-content-sm-start d-flex justify-content-center">
-          <ul class="nav nav-pills">
-            <li class="nav-item">
-              <download-button />
-            </li>
-          </ul>
-        </div>
+        <b-collapse id="menu" class="col-sm-auto d-sm-flex">
+          <div class="d-flex justify-content-center justify-content-sm-end">
+            <ul class="nav nav-pills flex-column flex-md-row">
+              <li class="nav-item ml-sm-3 mt-3 mt-sm-0">
+                <download-button />
+              </li>
+            </ul>
+          </div>
+        </b-collapse>
       </div>
     </nav>
   </div>
