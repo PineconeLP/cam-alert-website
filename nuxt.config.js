5cddcaf541589e399d20b1d0049eaf8c15f95fdf
PineconeLP <pineconelp4@gmail.com>
2020-08-12 20:39:14 -0400

    Fix favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,13 @@ export default {
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "Meta description" }
+    ],
+    link: [
+      {
+        rel: "icon",
+        type: "image/x-icon",
+        href: "favicon.ico"
+      }
     ]
   },
   css: ["@/css/main.css"],
